dd2ccd0ae68e091c44590f3d9cb74ae18705c692
sashavrg <168757785+sashavrg@users.noreply.github.com>
2024-10-08 15:40:39 +0200

    Updated to assignment 1.5
    chenged course, its parts and exercises into a single object, fixed references, made Total component procedural using .reduce

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,7 +2,7 @@ const Header = (props) => {
   console.log(props)
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </>
   )
 }
@@ -21,99 +21,51 @@ const Content = (props) => {
   console.log(props)
   return (
     <div>
-    <Part part={props.part1} exercises={props.exercises1}/>
-    <Part part={props.part2} exercises={props.exercises2}/>
-    <Part part={props.part3} exercises={props.exercises3}/>
+    <Part part={props.course.parts[0].name} exercises={props.course.parts[0].exercises}/>
+    <Part part={props.course.parts[1].name} exercises={props.course.parts[1].exercises}/>
+    <Part part={props.course.parts[2].name} exercises={props.course.parts[2].exercises}/>
    </div>
   )
 }
 
 const Total = (props) => {
   console.log(props)
+  const totalEx = props.course.parts.reduce((sum, part) => sum + part.exercises, 0)
   return (
     <>
-      <p>Number of exercises {props.total}</p>
+      <p>Number of exercises {totalEx}</p>
     </>
   )
 }
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <>
       <Header course={course} />
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      />
-      <Total total={exercises1 + exercises2 + exercises3}/>
+      <Content course={course} />
+      <Total course={course}/>
     </>
   )
 }
 
-
-/* I thought that the App component was looking kind of ugly doing it this way,  so I tried learning a little more about arrays and using them here
-
-
-const Part = (props) => {
-  console.log(props);
-  return (
-    <>
-      <p>
-        {props.part.name} {props.part.exercises}
-      </p>
-    </>
-  )
-}
-
-const Content = (props) => {
-  console.log(props);
-  return (
-    <div>
-      {props.parts.map((part) => (
-        <Part key={part.name} part={part} />
-      ))}
-    </div>
-  )
-}
-
-const Total = (props) => {
-  console.log(props);
-  return (
-    <>
-      <p>Number of exercises {props.total}</p>
-    </>
-  )
-}
-
-const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 },
-  ];
-
-  return (
-    <>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total total={parts.reduce((sum, part) => sum + part.exercises, 0)} />
-    </>
-  )
-}
-*/
 
 export default App
