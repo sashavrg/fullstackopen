9f3686d12a245669c1afaebdb3f9d54416b10120
sashavrg <168757785+sashavrg@users.noreply.github.com>
2024-10-30 10:04:12 +0100

    courseinfo App.jsx updated to first part of exercise 2.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,11 @@
+const Course = ({ course }) => {
+  return (
+    <div>
+    <Header course={course} />
+    <Content course={course} />
+    </div>)
+  }
+
 const Header = (props) => {
   console.log(props)
   return (
@@ -41,27 +49,30 @@ const Total = (props) => {
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
   return (
     <>
-      <Header course={course} />
-      <Content course={course} />
+      <Course course={course} />
       <Total course={course}/>
     </>
   )
