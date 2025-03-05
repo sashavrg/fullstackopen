86ca5300c36abbacce1e97e71579c0a508176c4d
sashavrg <168757785+sashavrg@users.noreply.github.com>
2025-03-05 15:54:26 +0100

    Completed exercise 3.7
    added 'morgan' middleware for logging requests, configured to use the 'tiny' format

--- part3/part3.1-3.6/index.js
+++ part3/part3.1-3.6/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const app = express()
+const morgan = require('morgan')
 
-app.use(express.json())
+const app = express()
 
 let persons = [
   { 
@@ -25,6 +25,22 @@ let persons = [
     "number": "39-23-6423122"
   }
 ]
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:', request.path)
+  console.log('Body', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(express.json())
+app.use(requestLogger)
+app.use(morgan('tiny'))
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 const generateId = () => {
   let newId
@@ -77,7 +93,7 @@ app.post('/api/persons', (request, response) => {
       error: 'Contact name must be unique!'
     })
   }
-  
+
   const person = {
     name : body.name,
     number : body.number,
